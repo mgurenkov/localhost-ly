b41338e5f91669a52e1d8c03175dfda05c7848e3
Michael <michael@gurenkov.net>
2016-03-03 12:58:59 +0300

    Добавлены классы для доступа к данным

---
+++ LocalhostLy/Model/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalhostLy.Model.Data
+{
+    class DataContext : DbContext 
+    {
+        public DbSet<LinkData> Links { get; set; }
+    }
+}
---
+++ LocalhostLy/Model/Data/LinkData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalhostLy.Model.Data
+{
+    public class LinkData
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Дата создания
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// Кто создал
+        /// </summary>
+        public Guid Author { get; set; }
+
+        /// <summary>
+        /// Коротка ссылка
+        /// </summary>
+        public string ShortLink { get; set; }
+
+        /// <summary>
+        /// Исходная ссылка
+        /// </summary>
+        public string OriginalLink { get; set; }
+
+        /// <summary>
+        /// Количество переходов
+        /// </summary>
+        public int LinkNavigations { get; set; }
+    }
+}
