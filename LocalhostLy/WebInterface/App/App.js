2bc93bc09c1f881dafcab59e4972a3feded21d7b
Michael <michael@gurenkov.net>
2016-03-03 17:25:21 +0300

    Верстка готова

---
+++ LocalhostLy/WebInterface/App/App.js
@@ -0,0 +1,34 @@
+﻿var theApp = angular.module('LocalhostLy', ['ui.router']);
+
+theApp.run(
+    ['$rootScope', '$state', '$stateParams',
+        function ($rootScope, $state, $stateParams) {
+
+        }
+    ]
+)
+
+// ---- ROUTING ----
+theApp.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.when("", "/");
+
+    $stateProvider
+        .state("root", {
+            url: "/",
+            templateUrl: '/App/index.html',
+            controller: 'IndexController'
+        })
+        .state("links", {
+            url: "/links",
+            templateUrl: '/App/links.html',
+            controller: 'LinksController'
+        });
+})
+
+theApp.controller('IndexController', function ($scope, $http) {
+
+});
+
+theApp.controller('LinksController', function ($scope, $http) {
+
+});
