fc5a548b4cf3f87cc474ac9ee7249272981c4a04
Michael <michael@gurenkov.net>
2016-03-04 16:28:03 +0300

    Добавлен локатор ресурсов

---
+++ LocalhostLy/Model/Registry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalhostLy.Model
+{
+    /// <summary>
+    /// Локатор ресурсов и реестр
+    /// </summary>
+    public static class Registry
+    {
+        public static ILinkService LinkService()
+        {
+            return new LinkService();
+        }
+    }
+}
