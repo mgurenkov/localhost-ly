22ebaf8d4729653a99e0426c11fa1ba9c83934c8
Michael <michael@gurenkov.net>
2016-03-04 16:41:09 +0300

    Сделан rest-интерфейс

---
+++ LocalhostLy/WebInterface/Controllers/LinksController.cs
@@ -0,0 +1,33 @@
+﻿using LocalhostLy.Model;
+using LocalhostLy.Model.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ThePlatform.Core.Data;
+using WebInterface.Models;
+
+namespace WebInterface.Controllers
+{
+    [RoutePrefix("api/links")]
+    public class LinksController : ApiController
+    {
+        [Route("")]
+        [HttpPost]
+        public CommandResult<LinkData> CreateLink(string OriginalLink)
+        {
+            var service = Registry.LinkService();
+            return service.Create(OriginalLink, Users.UserId());
+        }
+
+        [Route("")]
+        [HttpGet]
+        public LinkData[] List()
+        {
+            var service = Registry.LinkService();
+            return service.Find(Users.UserId());
+        }
+    }
+}
