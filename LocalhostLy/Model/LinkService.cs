dca1851f25b4e3ec5a99216c0f089b53b490354a
Michael <michael@gurenkov.net>
2016-03-04 15:25:41 +0300

    Реализация сервиса ссылок

--- LocalhostLy/Model/LinkService.cs
+++ LocalhostLy/Model/LinkService.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using ThePlatform.Common.Execution;
 using ThePlatform.Core.Data;
 
 namespace LocalhostLy.Model
@@ -43,22 +45,70 @@ namespace LocalhostLy.Model
     {
         public CommandResult<LinkData> Create(string a_OriginalUrl, Guid a_UserId)
         {
-            throw new NotImplementedException();
+            var r = new OperationResult();
+
+            if (string.IsNullOrWhiteSpace(a_OriginalUrl))
+            {
+                r.AddError("OriginalLink", "Не указана ссылка");
+                return new CommandResult<LinkData>(null, r);
+            }
+
+            if (!ValidateUrl(a_OriginalUrl))
+            {
+                r.AddError("OriginalLink", "Указана некорректная ссылка");
+                return new CommandResult<LinkData>(null, r);
+            }
+
+            using (var db = new DataContext())
+            {
+                var link = new LinkData()
+                {
+                    Author = a_UserId,
+                    CreatedAt = DateTime.Now,
+                    LinkNavigations = 0,
+                    OriginalLink = a_OriginalUrl,
+                };
+
+                db.Links.Add(link);
+                db.SaveChanges();
+
+                link.ShortLink = string.Format("l{0}", a_OriginalUrl.GetHashCode() ^ link.Id.GetHashCode());
+                db.SaveChanges();
+
+                return new CommandResult<LinkData>(link, r);
+            }
+        }
+
+        static Regex m_UrlValidator = new Regex(@"^(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%\$#_]*)?$", RegexOptions.Compiled);
+        bool ValidateUrl(string a_Url)
+        {
+            return m_UrlValidator.IsMatch(a_Url);
         }
 
         public LinkData Find(string a_ShortLink)
         {
-            throw new NotImplementedException();
+            using (var db = new DataContext())
+            {
+                return db.Links.FirstOrDefault(x => x.ShortLink == a_ShortLink);
+            }
         }
 
         public LinkData[] Find(Guid a_UserId)
         {
-            throw new NotImplementedException();
+            using (var db = new DataContext())
+            {
+                return db.Links.Where(x => x.Author == a_UserId).ToArray();
+            }
         }
 
         public void IncrementVisits(int a_Id)
         {
-            throw new NotImplementedException();
+            using (var db = new DataContext())
+            {
+                // статистика должна обновлять прямым запросом в базу данных, 
+                // иначе при работе через объектную модель между загрузкой данных и запросом на обновление кто-то еще может данные поменять.
+                db.Database.ExecuteSqlCommand("update Links set LinkNavigations = LinkNavigations + 1 where id = @p0", a_Id);
+            }
         }
     }
 }
